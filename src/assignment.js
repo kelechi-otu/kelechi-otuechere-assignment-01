462ba7ff7a59214de107c22cfade2e2dce6ea33c
kelechi-otu <99078997+kelechi-otu@users.noreply.github.com>
2022-07-15 20:28:53 +0000

    assignment done

--- src/assignment.js
+++ src/assignment.js
@@ -9,28 +9,39 @@ const assignment = {};
  * 
  * Calculate and return the sum of the numbers in an array.
  * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
+ 
+  @param {Array} arrayOfNumbers the array of numbers to sum
+  @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let sum = 0;
+    arrayOfNumbers.forEach(function(element) {
+        sum = sum + element;
+    });
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
  * 
  * Count all even numbers within an array of numbers.
  * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
+ 
+ @param {Array} arrayOfNumbers the array containing even or non-even numbers
+ @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+    arrayOfNumbers.forEach(function(element) {
+        let divisionResult = element / 2;
+        if (Number.isInteger(divisionResult)) {
+            count = count + 1;
+        }
+    })
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
